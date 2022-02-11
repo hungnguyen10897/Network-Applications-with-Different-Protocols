dcf3fe53784c3453371c3dd0b96632a48fefe9b5
Hung Nguyen <hungnguyen10897@gmail.com>
2022-02-11 13:28:08 +0200

    Add server in h2, client script using hyper and tweak logic

---
+++ http2/client.sh
@@ -0,0 +1,4 @@
+# Example request sent by client
+# GPS location in header
+
+hyper --h2 GET http://localhost:8080/ gps:'60.1694098 24.9373494'
--- http2/logic.py
+++ http2/logic.py
@@ -5,10 +5,17 @@ route = metadata['route']
 regions = json.load(open("./trace/regions.json"))
 
 
-def get_region(gps):
+def get_region_json_map(gps_str):
+
+    # Format GPS into compatible format with metadata
+    parts = gps_str.split(" ")
+    gps_tuple = [float(parts[0]), float(parts[1])]
+
+    # Look up GPS
     for index, loc in enumerate(route):
-        if gps != loc: continue
+        if gps_tuple != loc: continue
 
         for region, range in regions.items():
             if index >=  range["start"] and index <= range["end"]:
-                return region
+                return json.load(open(f"./trace/{region.lower()}.json"))
+
